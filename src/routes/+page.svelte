06e492c154126467df1d4295830f6213419f0fc8
Scott Mattera <scott.mattera34@gmail.com>
2025-10-01 14:55:40 -0400

    adding old site assets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,58 @@
-<h1>Scott's Site</h1>
+<head>
+    <title>Scott's Site</title>
+    <style>
+      a:link {
+        text-decoration: none;
+      }
 
-<h2>Scott's Website</h2>
+      a:visited {
+        text-decoration: none;
+      }
+
+      a:hover {
+        color: white;
+        background-color: black;
+      }
+
+      a:active {
+        color: white;
+        background-color: black;
+      }
+      header {
+        background-color: #0b8bb3;
+        padding: 20px;
+        text-align: left;
+        font-size: 35px;
+        color: white;
+      }
+
+
+
+      .button{
+        color: white;
+        width: 350px;
+        height: 50px;
+
+      }
+      .button1 {
+        background-image: url('image/background.png');
+        background-size: cover;
+        background-position: center;
+        }
+
+
+    </style>
+  </head>
+<body style="background-image: url('image/bgcand1.jpg'); background-size: cover;">  <!-- color:#29a8cf; -->
+<a href = "/work.html"> <button class = "button button1" type="button">Projects</button> </a>
+<a href = "/about.html"> <button class = "button button1" type="button">About Me</button> </a>
+<a href = "/other.html"> <button class = "button button1" type="button">Other Stuff</button> </a>
+<header> <h2>Scott's Website</h2> </header>
 <p>It's a website. </p>
 
 <img src="https://cyber.dabamos.de/88x31/aimlink.gif" alt="Great Graphic">
+
+</body>
 
 
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
