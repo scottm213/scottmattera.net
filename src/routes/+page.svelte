b2ab3839761b0acbfead5a1d3088a71701ed4933
Scott Mattera <scott.mattera34@gmail.com>
2025-10-02 11:17:04 -0400

    css style file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,9 @@
 <script>
 </script>
 
-<head>
-    <title>Scott's Site</title>
-    <style>
 
-      header {
-        background-color: #005bb7;
-          padding: 15px;
-        text-align: left;
-        font-size: 35px;
-        color: white;
-      }
+<title>Scott's Site</title>
 
-
-
-      .button{
-        color: white;
-        width: 200px;
-        height: 40px;
-          border: none;
-
-      }
-      .button1 {
-        background-color: #005bb7;
-        }
-
-      .button2 {
-          background-color: #003468;
-      }
-
-
-
-    </style>
-  </head>
 <body style="background-color:#3e3a55;">
 <a href = "https://scottmattera.net"> <button class = "button button1" type="button">Home</button> </a>
 <a href = "https://scottmattera.net/work"> <button class = "button button2" type="button">Projects</button> </a>
