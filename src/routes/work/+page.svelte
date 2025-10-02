d52e6ad1669dbd6b89318c403e03fcb24272549f
Scott Mattera <scott.mattera34@gmail.com>
2025-10-02 12:38:59 -0400

    fix links to other tabs

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -6,6 +6,6 @@
 
 <a href = "https://scottmattera.net"> <button class = "button button2" type="button">Home</button> </a>
 <a href = "https://scottmattera.net/work"> <button class = "button button1" type="button">Projects</button> </a>
-<a href = "/about.html"> <button class = "button button2" type="button">About Me</button> </a>
-<a href = "/other.html"> <button class = "button button2" type="button">Other Stuff</button> </a>
+<a href = "https://scottmattera.net/about"> <button class = "button button2" type="button">About Me</button> </a>
+<a href = "https://scottmattera.net/other"> <button class = "button button2" type="button">Other Stuff</button> </a>
 <header> <h2>My Work</h2> </header>
