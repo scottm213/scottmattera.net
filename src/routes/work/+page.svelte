11ad8a930def2ad81c6640f2fa4ef0c7f8c92d8a
Scott Mattera <scott.mattera34@gmail.com>
2025-10-07 13:14:22 -0400

    finally fixed buttons, and fixed sizing of many elements

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -1,22 +1,37 @@
 <script>
 
     let workOpen = $state(false);
-    import Tabs from "../tabs.svelte";
     function buttonClicked(){
         console.log("Clicked");
+        workOpen = !workOpen;
     }
 </script>
 
 <title>My Work</title>
 
 <div> <br> </div>
-<Tabs/>
+<a href = "https://scottmattera.net">
+    <button class = "button button2" type="button">Home</button> </a>
+<a href = "/work">
+    <button class = "button button1" type="button">Projects</button> </a>
+<a href = "/about">
+    <button class = "button button2" type="button">About Me</button> </a>
+<a href = "/other">
+    <button class = "button button2" type="button">Other Stuff</button> </a>
 <header> <h2>My Work</h2> </header>
-<div class = "square flex flex-col ">
+<div class = "square flex flex-col items-start justify-start">
+    <div class="grid grid-cols-4 gap-2 ">
 
-    <button  class = "mt-30 button button3" type="button" onclick={buttonClicked}>School Projects</button>
+        <button  class = " button button3 mt-20 col-start-3 col-end-4" type="button" onclick={buttonClicked}>School Projects</button>
 
 
-    <a href = "https://scottmattera.net/work"> <button class = "mt-80 button button3" type="button">Personal Projects</button> </a>
+        <button class = " button button3 mt-50 col-start-3 col-end-4" type="button">Personal Projects</button>
 
+
+    </div>
+    {#if workOpen}
+        <p class = "justify-start">
+            This text needs to be on the right.
+        </p>
+    {/if}
 </div>
