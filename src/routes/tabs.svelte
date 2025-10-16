e22388a673ae9f5d6fd071fbf97d7996483288c6
Scott Mattera <scott.mattera34@gmail.com>
2025-10-16 11:27:31 -0400

    tabs now is just one file and works BEAUTIFULLY

--- src/routes/tabs.svelte
+++ src/routes/tabs.svelte
@@ -2,9 +2,7 @@
     let currentButtonClass = "button button1"
     import { page } from '$app/state';
     function getCurrentPage(hrefItem){
-        console.log(page.url.pathname);
-        console.log(page.url.href);
-        if (page.url.href === hrefItem){
+        if (page.url.pathname === hrefItem){
             return currentButtonClass = "button button1";
         } else{
             return currentButtonClass = "button button2";
@@ -15,12 +13,12 @@
 
 
 <nav class="tabs">
-    <a href = "https://scottmattera.net">
-        <button class = {getCurrentPage("https://scottmattera.net")} type="button">Home</button> </a>
+    <a href = "/">
+        <button class = {getCurrentPage("/")} type="button">Home</button> </a>
     <a href = "/work">
-        <button class = {getCurrentPage("https://scottmattera.net/work")} type="button">Projects</button> </a>
+        <button class = {getCurrentPage("/work")} type="button">Projects</button> </a>
     <a href = "/about">
-        <button class = {getCurrentPage("https://scottmattera.net/about")} type="button">About Me</button> </a>
+        <button class = {getCurrentPage("/about")} type="button">About Me</button> </a>
     <a href = "/other">
-        <button class = {getCurrentPage("https://scottmattera.net/other")} type="button">Other Stuff</button> </a>
+        <button class = {getCurrentPage("/other")} type="button">Other Stuff</button> </a>
 </nav>
