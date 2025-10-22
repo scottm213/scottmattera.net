5494f82422bc8717fc1d4a4f4ba502a55c91e8dc
Scott Mattera <scott.mattera34@gmail.com>
2025-10-22 18:02:25 -0400

    funny update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,9 +46,9 @@
     <div class = "square">
         <p>It's a website. </p>
 
-        <img src="https://cyber.dabamos.de/88x31/aimlink.gif" alt="Great Graphic">
 
-        <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
 
 
     </div>
--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -48,10 +48,10 @@
     <div class = "square flex flex-col items-start justify-start">
         <div class="grid grid-cols-4 gap-2 ">
 
-            <button  class = "w-37.5 h-10 {buttonStyle1}  mt-20 col-start-3 col-end-4" type="button" value="school" onclick={schoolButtonClicked}>School Projects</button>
+            <button  class = "w-37.5 h-10 {buttonStyle1} rounded-tl-lg rounded-br-lg mt-20 col-start-3 col-end-4" type="button" value="school" onclick={schoolButtonClicked}>School Projects</button>
 
 
-            <button class = "w-37.5 h-10 {buttonStyle2} hover:bg-[#004381] mt-50 col-start-3 col-end-4" type="button" value="personal "onclick={personalButtonClicked}>Personal Projects</button>
+            <button class = "w-37.5 h-10 {buttonStyle2} rounded-tl-lg rounded-br-lg hover:bg-[#004381] mt-50 col-start-3 col-end-4" type="button" value="personal "onclick={personalButtonClicked}>Personal Projects</button>
         </div>
     </div>
         <div>
