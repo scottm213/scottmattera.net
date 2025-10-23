8974a636db6511a8a622b64c861419ea11b9806e
Scott Mattera <scott.mattera34@gmail.com>
2025-10-23 11:33:32 -0400

    adding new payload page for testing

--- src/routes/payload/+page.svelte
+++ src/routes/payload/+page.svelte
@@ -1,3 +1,18 @@
-<h1>
-    Hello! Secure ED is compromised!
-</h1>
+<script lang="ts">
+    let bPressed = $state(false);
+    function onBClick(){
+         bPressed = !bPressed;
+    }
+</script>
+
+
+<div>
+    <h1>
+        Hello! Secure ED is compromised!
+    </h1>
+    <button onclick = {onBClick}>Click to remove virus</button>
+
+    {#if bPressed}
+        <p>Virus</p>
+    {/if}
+</div>
