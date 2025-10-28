2719ca111be0e77c06c4865d3b346835b27eb324
Scott Mattera <scott.mattera34@gmail.com>
2025-10-28 14:20:21 -0400

    added buttons to links page

--- src/routes/other/+page.svelte
+++ src/routes/other/+page.svelte
@@ -12,6 +12,13 @@
         <p>Check out my sites!</p>
     </div>
     <div class = "otherSquare bg-[#004281]  ">
-        <a href = "https://github.com/scottm213"> Github </a>
+        <div class = "flex flex-col ">
+            <br>
+            <a href = "https://github.com/scottm213" target="_blank" class = "hover:bg-gray-200 text-3xl bg-gray-100 border-2 border-gray-200 text-gray-700 rounded h-15 w-35 inline-flex items-center">
+                <img src="/image/mark-github.svg" class = "h-10 w-10" alt="github logo">Github</a>
+            <br>
+            <a href = "https://www.linkedin.com/in/scott-mattera/" target="_blank" class = "hover:bg-gray-200 text-2xl bg-gray-100 border-2 border-gray-200 text-gray-700 rounded h-15 w-35 inline-flex items-center">
+                <img src="/image/LinkedIn_icon.svg" class = "h-10 w-10" alt="github logo">LinkedIn</a>
+        </div>
     </div>
 </div>
