d835db5be7e5cc281cacc5a806eeb87f65b8b22b
Scott Mattera <scott.mattera34@gmail.com>
2025-11-06 12:07:16 -0500

    size test

--- src/routes/museum/+page.svelte
+++ src/routes/museum/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <br>
-<button class="button1"> <a href="/demos">Go Back </a></button>
+<button class="button button1 w-20"> <a href="/demos">Go Back </a></button>
 <header> <h2> POV-Ray Renders</h2></header>
 <div>
 
